bf26b946c77718bfcb122f61a42fbd4631cda078
Mike Topskiy <36136553+broadnetty@users.noreply.github.com>
2018-02-04 23:49:39 +0300

    new tab opening
    I've figured out how to open new tab in the same window

--- Veeam.py
+++ Veeam.py
@@ -6,3 +6,7 @@ class TestexecCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.view.insert(edit, 0, "Hello, World!")
 
+class NewwinCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		self.window.new_file()
+
