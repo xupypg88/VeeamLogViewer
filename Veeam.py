cae9e140bd1bf7691596c185633b82f92ba7fc6a
Mike Topskiy <36136553+broadnetty@users.noreply.github.com>
2018-02-09 00:18:54 +0300

    found way to put text in new window and get sel text

--- Veeam.py
+++ Veeam.py
@@ -1,14 +1,30 @@
 import sublime
 import sublime_plugin
 
-
+buff='kjhj'
 class TestexecCommand(sublime_plugin.TextCommand):
+	def run(self, edit, lines):
+		currvw = self.view
+		texta=''
+		for line in lines:
+			texta += line + '\n'
+		self.view.insert(edit, 0, texta)
+
+class TextgetCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+		currvw = self.view
+		return currvw.substr(currvw.sel()[0])
 
 class NewwinCommand(sublime_plugin.WindowCommand):
 	def run(self):
-		id_new = self.window.new_file()
+
+		v = self.window.active_view()
+
+		buffa=[]		
+		for ln in v.sel():
+			buffa.append(v.substr(ln))
+
+		self.window.new_file()
 		viewv = self.window.active_view()
-		viewv.run_command('testexec')
+		viewv.run_command('testexec', { "lines" : buffa })
 
